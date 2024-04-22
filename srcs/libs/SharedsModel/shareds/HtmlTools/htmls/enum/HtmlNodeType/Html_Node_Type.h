637d2c4be1a2f5370f48953c1bb54dc304eb20f8
Acreate <1552397900@qq.com>
2024-04-22 18:23:51 +0800

    初次提交

---
+++ srcs/libs/SharedsModel/shareds/HtmlTools/htmls/enum/HtmlNodeType/Html_Node_Type.h
@@ -0,0 +1,16 @@
+﻿#ifndef HTML_NODE_TYPE_H_H_HEAD__FILE__
+#define HTML_NODE_TYPE_H_H_HEAD__FILE__
+#pragma once
+
+#include "../../../nameSpace/HtmlTools.h"
+namespace HtmlTools {
+	enum class HTMLTOOLS_EXPORT Html_Node_Type {
+		None,
+		DoubleNode,
+		SingleNode,
+		AnnotationNode
+	};
+
+}
+
+#endif // HTML_NODE_TYPE_H_H_HEAD__FILE__
---
+++ srcs/projects/testHtmlCpp/src/main/main.cpp
@@ -0,0 +1,57 @@
+#include <codecvt>
+
+#include "../macro/cmake_to_c_cpp_header_env.h"
+#include <fstream>
+#include <htmls/HtmlNode/HtmlNode.h>
+#include <htmls/htmlDoc/HtmlDoc.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+
+int main( int argc, char *argv[ ] ) {
+	std::string fString( u8"%s/%s/%s" );
+	char path[ 4096 ]{ 0 };
+	int len = snprintf( path, sizeof( path ), fString.c_str( ), std::string( Project_Run_bin ).c_str( ), u8"writeFile", u8"www.121ds.cc.html" );
+	if( len == 0 )
+		return -1;
+	std::wifstream ifstream( path, std::ios::binary | std::ios::in );
+	if( !ifstream.is_open( ) )
+		return -2;
+
+	std::wstringstream stringstream;
+	do {
+		std::wstring getStr;
+		auto &getline = std::getline( ifstream, getStr );
+		if( getline.eof( ) )
+			break;
+		if( !getStr.empty( ) )
+			stringstream << getStr;
+		stringstream << L'\n';
+	} while( true );
+
+	std::shared_ptr< std::wstring > htmlContent( new std::wstring( stringstream.str( ) ) );
+	//std::wcout << *htmlContent << std::endl;
+
+	ifstream.close( );
+	// 写入文件
+	len = snprintf( path, sizeof( path ), fString.c_str( ), std::string( Cache_Path_Dir ).c_str( ), u8"", u8"www.121ds.cc.txt" );
+	if( len == 0 )
+		return -1;
+	std::wofstream ofstream( path, std::ios::binary | std::ios::out | std::ios::trunc );
+	if( !ofstream.is_open( ) )
+		return -2;
+	ofstream << stringstream.str( );
+	ofstream.close( );
+
+	// 测试调用
+	size_t endIndex = htmlContent->size( ), startIndex = 0;
+	auto htmlDoc = HtmlTools::HtmlDoc::parse( htmlContent, endIndex, startIndex );
+	htmlDoc.analysisBrotherNode( );
+	htmlDoc.getNodes( [&]( auto node ) {
+		auto nodePath = *node->getPath( );
+		std::wcout << nodePath << std::endl;
+		return false;
+	} );
+	return 0;
+}
