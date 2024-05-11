2b4bc00f67d19038c5586442f570e63b0d9d3618
Acreate <1552397900@qq.com>
2024-05-11 14:50:52 +0800

    工作线程增加 std::shared_ptr 类型

--- srcs/HtmlTools/htmls/htmlTools/HtmlWorkThread/HtmlWorkThread.h
+++ srcs/HtmlTools/htmls/htmlTools/HtmlWorkThread/HtmlWorkThread.h
@@ -16,6 +16,7 @@ namespace cylHtmlTools {
 	template< class TDataType >
 	class HTMLTOOLS_EXPORT HtmlWorkThread {
 	};
+
 	template< class TDataType >
 	class HTMLTOOLS_EXPORT HtmlWorkThread< TDataType * > {
 	public:
@@ -35,9 +36,9 @@ namespace cylHtmlTools {
 			, Run // 正在运行
 		};
 	private:
-		T_Start_Thread_Run< TDataType * > startThreadRun; // 线程开始之后调用
-		T_Current_Thread_Run< TDataType * > currentThreadRun; // 正式工作
-		T_Finish_Thread_Run< TDataType * > finishThreadRun; // 线程完成之后调用
+		Start_Thread_Run startThreadRun; // 线程开始之后调用
+		Current_Thread_Run currentThreadRun; // 正式工作
+		Finish_Thread_Run finishThreadRun; // 线程完成之后调用
 		std::thread *thread; // 线程对象
 		Html_Work_Status workStatus; // 记录线程状态
 		time_t *startTime; // 记录线程开始的运行时间
@@ -64,6 +65,75 @@ namespace cylHtmlTools {
 		/// <param name="data">传递的数据指针</param>
 		/// <returns>成功返回 true</returns>
 		bool reInit( const Start_Thread_Run &start_thread_run, const Current_Thread_Run &current_thread_run, const Finish_Thread_Run &finish_thread_run, TDataType *data );
+		/// <summary>
+		/// 线程开始工作
+		/// </summary>
+		void start( );
+		/// <summary>
+		/// 是否正在运行
+		/// </summary>
+		/// <returns>运行返回 true</returns>
+		bool isRun( ) const;
+		/// <summary>
+		/// 是否运行结束
+		/// </summary>
+		/// <returns>结束返回 true</returns>
+		bool isFinish( ) const;
+		/// <summary>
+		/// 等待线程结束
+		/// </summary>
+		/// <returns>线程运行时间</returns>
+		time_t wait( );
+	};
+
+	template< class TDataType >
+	class HTMLTOOLS_EXPORT HtmlWorkThread< std::shared_ptr< TDataType > > {
+	public:
+		template< class TThreadDataType >
+		using T_Start_Thread_Run = std::function< void( const HtmlWorkThread *html_work_thread, const std::thread *run_std_cpp_thread, std::mutex *html_work_thread_mutex, std::mutex *std_cpp_thread_mutex, TThreadDataType &data ) >;
+		template< class TThreadDataType >
+		using T_Current_Thread_Run = std::function< void( const HtmlWorkThread *html_work_thread, const std::thread *run_std_cpp_thread, std::mutex *html_work_thread_mutex, std::mutex *std_cpp_thread_mutex, TThreadDataType &data, const time_t *startTime ) >;
+		template< class TThreadDataType >
+		using T_Finish_Thread_Run = std::function< void( const HtmlWorkThread *html_work_thread, const std::thread *run_std_cpp_thread, std::mutex *html_work_thread_mutex, std::mutex *std_cpp_thread_mutex, TThreadDataType &data ) >;
+		using Start_Thread_Run = T_Start_Thread_Run< std::shared_ptr< TDataType > >;
+		using Current_Thread_Run = T_Current_Thread_Run< std::shared_ptr< TDataType > >;
+		using Finish_Thread_Run = T_Finish_Thread_Run< std::shared_ptr< TDataType > >;
+		enum Html_Work_Status {
+			None // 没有任何状态-无法使用
+			, Init // 正在初始化
+			, Finish // 已经完成任务
+			, Run // 正在运行
+		};
+	private:
+		Start_Thread_Run startThreadRun; // 线程开始之后调用
+		Current_Thread_Run currentThreadRun; // 正式工作
+		Finish_Thread_Run finishThreadRun; // 线程完成之后调用
+		std::thread *thread; // 线程对象
+		Html_Work_Status workStatus; // 记录线程状态
+		time_t *startTime; // 记录线程开始的运行时间
+		time_t *endTime; // 记录线程结束的运行时间
+		std::mutex *mutexHtmlWorkThread; // 对象线程锁
+		std::mutex *mutexStdThread; // 线程锁
+		std::shared_ptr< TDataType > data; // 数据指针
+	public:
+		/// <summary>
+		/// 初始化一个对象
+		/// </summary>
+		/// <param name="start_thread_run">线程开始回调函数</param>
+		/// <param name="finish_thread_run">线程结束回调函数</param>
+		/// <param name="current_thread_run">线程正式工作的回调函数</param>
+		/// <param name="data">传递的数据指针</param>
+		HtmlWorkThread( const Start_Thread_Run &start_thread_run, const Current_Thread_Run &current_thread_run, const Finish_Thread_Run &finish_thread_run, std::shared_ptr< TDataType > &data );
+		virtual ~HtmlWorkThread( );
+		/// <summary>
+		/// 重新初始化线程
+		/// </summary>
+		/// <param name="start_thread_run">线程开始回调函数</param>
+		/// <param name="finish_thread_run">线程结束回调函数</param>
+		/// <param name="current_thread_run">线程正式工作的回调函数</param>
+		/// <param name="data">传递的数据指针</param>
+		/// <returns>成功返回 true</returns>
+		bool reInit( const Start_Thread_Run &start_thread_run, const Current_Thread_Run &current_thread_run, const Finish_Thread_Run &finish_thread_run, std::shared_ptr< TDataType > &data );
 		/// <summary>
 		/// 线程开始工作
 		/// </summary>
--- srcs/HtmlTools/htmls/htmlTools/HtmlWorkThread/HtmlWorkThread.impl.cpp
+++ srcs/HtmlTools/htmls/htmlTools/HtmlWorkThread/HtmlWorkThread.impl.cpp
@@ -80,6 +80,84 @@ time_t cylHtmlTools::HtmlWorkThread< TDataType * >::wait( ) {
 	this->mutexHtmlWorkThread->unlock( );
 	return result;
 }
+
+
+template< class TDataType >
+cylHtmlTools::HtmlWorkThread< std::shared_ptr< TDataType > >::HtmlWorkThread( const Start_Thread_Run &start_thread_run, const Current_Thread_Run &current_thread_run, const Finish_Thread_Run &finish_thread_run, std::shared_ptr< TDataType > &data ) {
+	this->workStatus = Init;
+	mutexHtmlWorkThread = new std::mutex( );
+	mutexStdThread = new std::mutex( );
+	this->endTime = nullptr;
+	this->startTime = nullptr;
+	this->startThreadRun = start_thread_run;
+	this->currentThreadRun = current_thread_run;
+	this->finishThreadRun = finish_thread_run;
+	this->data = data;
+}
+template< class TDataType >
+cylHtmlTools::HtmlWorkThread< std::shared_ptr< TDataType > >::~HtmlWorkThread( ) {
+	delete mutexHtmlWorkThread;
+	delete mutexStdThread;
+	if( thread ) {
+		thread->join( );
+		delete thread;
+	}
+}
+template< class TDataType >
+bool cylHtmlTools::HtmlWorkThread< std::shared_ptr< TDataType > >::reInit( const Start_Thread_Run &start_thread_run, const Current_Thread_Run &current_thread_run, const Finish_Thread_Run &finish_thread_run, std::shared_ptr< TDataType > &data ) {
+	if( isRun( ) || this->workStatus == None )
+		return false;
+	this->mutexHtmlWorkThread->lock( );
+	this->startThreadRun = start_thread_run;
+	this->currentThreadRun = current_thread_run;
+	this->finishThreadRun = finish_thread_run;
+	this->data = data;
+	this->mutexHtmlWorkThread->unlock( );
+	return true;
+}
+template< class TDataType >
+void cylHtmlTools::HtmlWorkThread< std::shared_ptr< TDataType > >::start( ) {
+	this->mutexHtmlWorkThread->lock( );
+	this->workStatus = Run;
+	this->thread = new std::thread( [this]( ) {
+		this->startTime = new time_t( std::time( nullptr ) );
+		this->startThreadRun( this, this->thread, this->mutexHtmlWorkThread, this->mutexStdThread, this->data );
+		this->currentThreadRun( this, this->thread, this->mutexHtmlWorkThread, this->mutexStdThread, this->data, this->startTime );
+		this->finishThreadRun( this, this->thread, this->mutexHtmlWorkThread, this->mutexStdThread, this->data );
+		this->endTime = new time_t( std::time( nullptr ) );
+		this->workStatus = Finish;
+	} );
+	this->mutexHtmlWorkThread->unlock( );
+}
+template< class TDataType >
+bool cylHtmlTools::HtmlWorkThread< std::shared_ptr< TDataType > >::isRun( ) const {
+	this->mutexHtmlWorkThread->lock( );
+	bool cond = this->workStatus == Run;
+	this->mutexHtmlWorkThread->unlock( );
+	return cond;
+}
+template< class TDataType >
+bool cylHtmlTools::HtmlWorkThread< std::shared_ptr< TDataType > >::isFinish( ) const {
+	this->mutexHtmlWorkThread->lock( );
+	bool cond = this->workStatus == Finish;
+	this->mutexHtmlWorkThread->unlock( );
+	return cond;
+}
+template< class TDataType >
+time_t cylHtmlTools::HtmlWorkThread< std::shared_ptr< TDataType > >::wait( ) {
+	if( isFinish( ) ) {
+		this->mutexHtmlWorkThread->lock( );
+		auto result = *this->endTime - *this->startTime;
+		this->mutexHtmlWorkThread->unlock( );
+		return result;
+	}
+	this->mutexHtmlWorkThread->lock( );
+	thread->join( );
+	auto result = *this->endTime - *this->startTime;
+	this->mutexHtmlWorkThread->unlock( );
+	return result;
+}
+
 template class cylHtmlTools::HtmlWorkThread< void * >;
 
 template class cylHtmlTools::HtmlWorkThread< char * >;
@@ -97,12 +175,37 @@ template class cylHtmlTools::HtmlWorkThread< unsigned long long * >;
 template class cylHtmlTools::HtmlWorkThread< double * >;
 template class cylHtmlTools::HtmlWorkThread< float * >;
 
+
 template class cylHtmlTools::HtmlWorkThread< cylHtmlTools::HtmlString * >;
 template class cylHtmlTools::HtmlWorkThread< cylHtmlTools::HtmlNode * >;
 template class cylHtmlTools::HtmlWorkThread< cylHtmlTools::HtmlDoc * >;
 template class cylHtmlTools::HtmlWorkThread< cylHtmlTools::XDir * >;
 template class cylHtmlTools::HtmlWorkThread< cylHtmlTools::XDirAttribute * >;
 template class cylHtmlTools::HtmlWorkThread< cylHtmlTools::XPath * >;
+
+
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< char > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< short > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< int > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< long > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< long long > >;
+
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< unsigned char > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< unsigned short > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< unsigned int > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< unsigned long > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< unsigned long long > >;
+
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< double > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< float > >;
+
+
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< cylHtmlTools::HtmlString > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< cylHtmlTools::HtmlNode > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< cylHtmlTools::HtmlDoc > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< cylHtmlTools::XDir > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< cylHtmlTools::XDirAttribute > >;
+template class cylHtmlTools::HtmlWorkThread< std::shared_ptr< cylHtmlTools::XPath > >;
 
 
 #endif // HTMLWORKTHREAD_CPP_IMPL_H_H_HEAD__FILE__
