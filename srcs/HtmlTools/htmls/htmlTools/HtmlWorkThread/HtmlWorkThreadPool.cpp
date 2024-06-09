f9693c08ad5217f73118b129d00e8d2f07e3784e
Acreate <1552397900@qq.com>
2024-06-09 12:20:47 +0800

    增加线程池类

---
+++ srcs/HtmlTools/htmls/htmlTools/HtmlWorkThread/HtmlWorkThreadPool.cpp
@@ -0,0 +1,151 @@
+﻿#include "HtmlWorkThreadPool.h"
+
+
+cylHtmlTools::HtmlWorkThreadPool::HtmlWorkThreadPool( ) {
+	this->workStatus = HtmlWorkThread::Init;
+	mutexHtmlWorkThread = new std::mutex;
+	workCount = 0;
+	currentWorkThread = 0;
+}
+cylHtmlTools::HtmlWorkThreadPool::~HtmlWorkThreadPool( ) {
+	delete mutexHtmlWorkThread;
+}
+bool cylHtmlTools::HtmlWorkThreadPool::isRun( ) const {
+	mutexHtmlWorkThread->lock( );
+	bool result = workStatus == HtmlWorkThread::Run;
+	mutexHtmlWorkThread->unlock( );
+	return result;
+}
+bool cylHtmlTools::HtmlWorkThreadPool::isFinish( ) const {
+	mutexHtmlWorkThread->lock( );
+	bool result = workStatus == HtmlWorkThread::Finish;
+	mutexHtmlWorkThread->unlock( );
+	return result;
+}
+bool cylHtmlTools::HtmlWorkThreadPool::isInit( ) const {
+	mutexHtmlWorkThread->lock( );
+	bool result = workStatus == HtmlWorkThread::Init;
+	mutexHtmlWorkThread->unlock( );
+	return result;
+}
+bool cylHtmlTools::HtmlWorkThreadPool::isNone( ) const {
+	mutexHtmlWorkThread->lock( );
+	bool result = workStatus == HtmlWorkThread::None;
+	mutexHtmlWorkThread->unlock( );
+	return result;
+}
+bool cylHtmlTools::HtmlWorkThreadPool::appendWork( const HtmlWorkThread::TThreadCall &work ) {
+	if( isRun( ) )
+		return false;
+	mutexHtmlWorkThread->lock( );
+	works.emplace_back( work );
+	mutexHtmlWorkThread->unlock( );
+	return true;
+}
+void cylHtmlTools::HtmlWorkThreadPool::start( const cylHtmlTools::HtmlWorkThreadPool::TThreadCall &function_call ) {
+	if( workCount == 0 )
+		start( 8, function_call );
+	else
+		start( workCount, function_call );
+}
+void cylHtmlTools::HtmlWorkThreadPool::start( const size_t work_count, const cylHtmlTools::HtmlWorkThreadPool::TThreadCall &function_call ) {
+	if( this->workStatus == HtmlWorkThread::Run )
+		return;
+	workCount = work_count;
+	taskDistributeThread.setCurrentThreadRun( [this]( HtmlWorkThread * ) {
+
+		this->mutexHtmlWorkThread->lock( );
+		// 清理任务
+		if( workThreads.size( ) > 0 ) {
+			for( auto thread : workThreads ) {
+				if( !thread->isFinish( ) )
+					thread->wait( );
+				delete thread;
+			}
+			workThreads.clear( );
+		}
+		currentWorkThread = 0;
+		this->mutexHtmlWorkThread->unlock( );
+		size_t size = works.size( ); // 统计待工作队列大小
+		// 初始化工作线程
+		for( ; currentWorkThread < size; ) {
+			if( currentWorkThread == workCount )
+				break;
+			auto currentCheckThread = new HtmlWorkThread;
+			currentCheckThread->setCurrentThreadRun( works.at( 0 ) );
+			currentCheckThread->setFinishThreadRun( [&]( HtmlWorkThread * ) {
+				this->mutexHtmlWorkThread->lock( );
+				--currentWorkThread;
+				this->mutexHtmlWorkThread->unlock( );
+			} );
+			workThreads.emplace_back( currentCheckThread );
+			this->mutexHtmlWorkThread->lock( );
+			++currentWorkThread;
+			this->mutexHtmlWorkThread->unlock( );
+			works.erase( works.begin( ) );
+		}
+		for( auto thread : workThreads )
+			thread->start( );
+		do {
+			size = works.size( ); // 统计待工作队列大小
+			if( size == 0 )
+				break;
+			std::vector< HtmlWorkThread * > workBuffThreads; // 当前工作线程
+			if( currentWorkThread < workCount && size != 0 ) { // 数量不足时，需要填充任务
+				auto threadsIterator = workThreads.begin( );
+				auto threadsEnd = workThreads.end( );
+				for( ; threadsIterator != threadsEnd && size != 0; ++threadsIterator ) { // 找一个空闲线程进行工作
+					auto currentCheckThread = *threadsIterator;
+					if( currentCheckThread->isFinish( ) ) {
+						currentCheckThread->setCurrentThreadRun( works.at( 0 ) );
+						currentCheckThread->setFinishThreadRun( [&]( HtmlWorkThread * ) {
+							this->mutexHtmlWorkThread->lock( );
+							--currentWorkThread;
+							this->mutexHtmlWorkThread->unlock( );
+						} );
+						this->mutexHtmlWorkThread->lock( );
+						works.erase( works.begin( ) );
+						--size;
+						++currentWorkThread;
+						this->mutexHtmlWorkThread->unlock( );
+						workBuffThreads.emplace_back( currentCheckThread );
+					}
+				}
+			}
+			for( auto thread : workBuffThreads )
+				thread->start( );
+		} while( true );
+		auto threadsIterator = workThreads.begin( );
+		auto threadsEnd = workThreads.end( );
+		for( ; threadsIterator != threadsEnd; ++threadsIterator ) {
+			HtmlWorkThread *htmlWorkThread = *threadsIterator;
+			htmlWorkThread->wait( );
+			delete htmlWorkThread;
+		}
+		workThreads.clear( );
+	} );
+
+	userCallThread.setCurrentThreadRun( [function_call,this]( HtmlWorkThread * ) {
+		while( true ) {
+			this->mutexHtmlWorkThread->lock( );
+			size_t size = works.size( );
+			this->mutexHtmlWorkThread->unlock( );
+			if( function_call )
+				function_call( this, size, currentWorkThread );
+			if( size == 0 )
+				break;
+		}
+	} );
+
+	taskDistributeThread.start( );
+	userCallThread.start( );
+}
+void cylHtmlTools::HtmlWorkThreadPool::waiteOverJob( ) {
+	if( taskDistributeThread.isInit( ) ) // 处理线程还没开始工作，则返回
+		return;
+	while( !taskDistributeThread.isFinish( ) ); // 处理线程正在工作，则停留
+	while( !userCallThread.isFinish( ) ); // 用户线程正在工作，则停留
+	mutexHtmlWorkThread->lock( );
+	workStatus = HtmlWorkThread::Finish; // 任务完成
+	mutexHtmlWorkThread->unlock( );
+}
---
+++ srcs/HtmlTools/htmls/htmlTools/HtmlWorkThread/HtmlWorkThreadPool.h
@@ -0,0 +1,78 @@
+﻿#ifndef HTMLWORKTHREADPOOL_H_H_HEAD__FILE__
+#define HTMLWORKTHREADPOOL_H_H_HEAD__FILE__
+#pragma once
+
+#include <functional>
+#include <mutex>
+#include <random>
+#include "HtmlWorkThread.h"
+
+namespace cylHtmlTools {
+	class HTMLTOOLS_EXPORT HtmlWorkThreadPool {
+	public:
+		/// <summary>
+		///	轮询回调函数
+		/// 1。当前工作线程
+		/// 2。剩余工作数
+		///	3。正在工作数
+		/// </summary>
+		using TThreadCall = std::function< void( HtmlWorkThreadPool *, const size_t &, const size_t & ) >;
+	private:
+		/// <summary>
+		/// 工作对象
+		/// </summary>
+		std::vector< HtmlWorkThread::TThreadCall > works;
+		// 当前工作线程
+		std::vector< HtmlWorkThread * > workThreads;
+		/// <summary>
+		/// 工作锁
+		/// </summary>
+		std::mutex *mutexHtmlWorkThread;
+		/// <summary>
+		/// 状态
+		/// </summary>
+		HtmlWorkThread::Html_Work_Status workStatus;
+		/// <summary>
+		/// 任务派发线程
+		/// </summary>
+		cylHtmlTools::HtmlWorkThread taskDistributeThread;
+		/// <summary>
+		/// 用户调用线程
+		/// </summary>
+		cylHtmlTools::HtmlWorkThread userCallThread;
+		// 工作数量
+		size_t workCount;
+		size_t currentWorkThread;  // 当前工作线程数量
+	public:
+		HtmlWorkThreadPool( );
+		virtual ~HtmlWorkThreadPool( );
+		size_t getWorkCount( ) const { return workCount; }
+		void setWorkCount( const size_t work_count ) { workCount = work_count; }
+		bool isRun( ) const;
+		bool isFinish( ) const;
+		bool isInit( ) const;
+		bool isNone( ) const;
+		/// <summary>
+		/// 追加一个任务
+		/// </summary>
+		/// <param name="work">追加的任务</param>
+		bool appendWork( const HtmlWorkThread::TThreadCall &work );
+		/// <summary>
+		/// 开始任务，每次轮询都会调用一次 function_call
+		/// </summary>
+		/// <param name="function_call">轮询调用函数</param>
+		void start( const cylHtmlTools::HtmlWorkThreadPool::TThreadCall &function_call );
+		/// <summary>
+		/// 设置任务数量，并且开始任务，每次轮询都会调用一次 function_call
+		/// </summary>
+		/// <param name="work_count">任务数量</param>
+		/// <param name="function_call">轮训回调函数</param>
+		void start( const size_t work_count, const cylHtmlTools::HtmlWorkThreadPool::TThreadCall &function_call );
+		/// <summary>
+		/// 等待任务总结
+		/// </summary>
+		void waiteOverJob( );
+	};
+}
+
+#endif // HTMLWORKTHREADPOOL_H_H_HEAD__FILE__
